6cb8336de934819878677421cda1f991528c1242
shahanshah <elifshah17@gmail.com>
2024-01-28 14:17:44 +0530

    Initial commit

---
+++ src/main/java/com/shah/learn/KafkaProducerLearningApplication.java
@@ -0,0 +1,13 @@
+package com.shah.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shah/learn/KafkaProducerLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shah.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
